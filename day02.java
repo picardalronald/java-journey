f40cb17d77bc2bbb6fad8b1dd1dab2a27f69808a
Ronald/ <picardalronald3@gmail.com>
2025-10-02 22:36:42 +0800

    Create day02.java

---
+++ day02.java
@@ -0,0 +1,13 @@
+//declare 2 string variables, 1 is hello, 1 is world, concatenate to say hello world
+
+public class day02 {
+    public static void main(String[] args) {
+
+    String hello = "Hello";
+
+    String world = "World!";
+
+        System.out.println(hello + " " + world);
+
+    }
+}
