ebfde7aac96a2acc3664d8ef03466e773b2bf3b7
Ronald/ <picardalronald3@gmail.com>
2025-10-02 22:42:59 +0800

    Create day03.java

---
+++ day03.java
@@ -0,0 +1,13 @@
+//Trim whitespace from strings using built in function
+public class day03 {
+    public static void main(String[] args) {
+
+    String hello = "   Hello World    ";
+
+    String trimmer  = hello.trim();
+
+        System.out.println(trimmer.trim());
+        System.out.println(hello);
+
+    }
+}
