a8a6fdf6239989810cc6c0c9a1182f2bf365a7da
Ronald/ <picardalronald3@gmail.com>
2025-10-03 20:43:49 +0800

    Create day04.java

---
+++ day04.java
@@ -0,0 +1,13 @@
+//uppercase-all and lowercase-all strings using built in function
+
+public class day04 {
+    public static void main(String[] args) {
+   
+    String name1 = "ronald";
+    String name2 = "RONALD";
+
+    System.out.println(name1.toUpperCase());
+    System.out.println(name2.toLowerCase());
+
+    }
+}
