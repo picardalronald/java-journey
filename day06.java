d36c38748c2aaac219588bb53bba99ccd448ba1b
Ronald/ <picardalronald3@gmail.com>
2025-10-04 16:21:29 +0800

    Create day06.java

---
+++ day06.java
@@ -0,0 +1,15 @@
+//concatenate integer and string without getting an error
+public class day06 {
+    public static void main(String[] args) {
+
+     String age = "Twenty One";
+     int ages = 21;
+
+
+    System.out.println("Your old are: " + ages +" " +
+     "Here the text Version: " + age);
+
+    }
+} 
+    
+
