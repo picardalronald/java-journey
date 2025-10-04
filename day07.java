6e58bdb4baee6398deb8c8b09dff3793caf9050b
Ronald/ <picardalronald3@gmail.com>
2025-10-04 17:05:13 +0800

    Create day07.java

---
+++ day07.java
@@ -0,0 +1,17 @@
+//concatenate float and string without getting an error
+public class day07 {
+    public static void main(String[] args) {
+
+     String grade = "Seventy Five point Five";
+     float grades = 75.5f;
+
+
+    System.out.println("Your Grade are: " + grades + " " + 
+    "Here the text Version: " + grade);
+
+    }
+} 
+    
+
+
+
