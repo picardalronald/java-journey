4b1ec3e9241638d9c4e5e603551494c1cc86996f
Ronald/ <picardalronald3@gmail.com>
2025-10-05 09:49:33 +0800

    Create day08.java

---
+++ day08.java
@@ -0,0 +1,16 @@
+//contatenate boolean and string without getting an error
+
+public class day08 {
+    public static void main(String[] args) {
+
+     String animals = "Cat is a animal and so cute";
+     boolean animal = true;
+
+
+    System.out.println("Output here: " + animals + " " + 
+    "Boolean Ouput: " + animal);
+
+    }
+} 
+    
+
