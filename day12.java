e0540265d5fbf0657f8a174c106cdef2fbc199b6
Ronald/ <picardalronald3@gmail.com>
2025-10-16 14:19:20 +0800

    Create day12.java

---
+++ day12.java
@@ -0,0 +1,12 @@
+// loop through a numeric array preferrably not using manual index increment
+
+public class day12 {
+    public static void main(String[] args) {
+
+      int[] numbers = {10, 20, 30, 40, 50};
+        for(int num : numbers) {
+          System.out.println(num);
+        }
+      
+    }
+} 
