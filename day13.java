1b23dcf70fbb035b4f5b34bfe27e7c8f9f0a37cd
Ronald/ <picardalronald3@gmail.com>
2025-10-17 15:28:22 +0800

    Create day13.java

---
+++ day13.java
@@ -0,0 +1,15 @@
+// loop through a numeric array printing both the index and the value
+
+public class day13 {
+    public static void main(String[] args) {
+
+      
+      int[] numbers = {10, 20, 30, 40, 50};
+
+      for (int i = 0; i < numbers.length; i++) {
+        System.out.println("Index: " + i + " Value: " + numbers[i] );
+
+      }
+      
+    }
+} 
